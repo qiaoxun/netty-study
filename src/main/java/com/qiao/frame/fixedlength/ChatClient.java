7d525bf818de4ffa12a72b1fbb0a4e23a6804721
Joey <woqiaoxun@163.com>
2019-08-04 11:36:58 +0800

    FixedLengthFrameDecoder

---
+++ src/main/java/com/qiao/frame/fixedlength/ChatClient.java
@@ -0,0 +1,65 @@
+package com.qiao.frame.fixedlength;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+
+public class ChatClient {
+
+    public static void main(String[] args) throws IOException {
+        String host = "127.0.0.1";
+        int port = 8888;
+        new ChatClient(host, port).run();
+    }
+
+    private final String host;
+    private final int port;
+
+    public ChatClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run() throws IOException {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .channel(NioSocketChannel.class)
+                    .handler(new ClientChannelInitializer());
+
+            Channel channel = bootstrap.connect().channel();
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//            for (int i = 0; i < 10; i++) {
+
+//            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            channel.writeAndFlush("this is a string");
+            while (true) {
+                String line = br.readLine();
+                if (null != line && line.equals("exit"))
+                    break;
+                channel.writeAndFlush(line);
+            }
+        } finally {
+            group.shutdownGracefully();
+        }
+
+    }
+}
--- src/main/java/com/qiao/frame/fixedlength/ClientChannelInitializer.java
+++ src/main/java/com/qiao/frame/fixedlength/ClientChannelInitializer.java
@@ -1,10 +1,11 @@
-package com.qiao.frame.delimiter;
+package com.qiao.frame.fixedlength;
 
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.handler.logging.LogLevel;
@@ -16,7 +17,7 @@ public class ClientChannelInitializer extends ChannelInitializer<SocketChannel> 
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
         pipeline.addLast(new LoggingHandler(LogLevel.DEBUG));
-        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(1024, Unpooled.copiedBuffer("$_", CharsetUtil.UTF_8)));
+        pipeline.addLast("framer", new FixedLengthFrameDecoder(5));
         pipeline.addLast("decoder", new StringDecoder());
         pipeline.addLast("encoder", new StringEncoder());
         pipeline.addLast("handler", new ChatClientInboundHandler());
